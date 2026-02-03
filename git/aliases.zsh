083c406dd4ebd402b37d1543b5b7228b08fb968d
Adrià Navarrete <adri.navarrete.09@gmail.com>
2026-02-03 16:13:36 +0100

    feat: add alias for git command

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -30,6 +30,7 @@ git_commit() {
 
 
 # The rest of my fun git aliases
+alias g='git'
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
