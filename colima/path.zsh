c6d8a03265b78ba515db45e06f516ad509a66839
Adrià Navarrete <adri.navarrete.09@gmail.com>
2026-02-04 08:28:58 +0100

    fix: conditionally set DOCKER_HOST if colima socket exists

--- colima/path.zsh
+++ colima/path.zsh
@@ -1,2 +1,4 @@
-# colima docker host
-export DOCKER_HOST="unix://${HOME}/.colima/default/docker.sock"
+# colima docker host - only set if colima socket exists
+if [[ -S "${HOME}/.colima/default/docker.sock" ]]; then
+  export DOCKER_HOST="unix://${HOME}/.colima/default/docker.sock"
+fi
