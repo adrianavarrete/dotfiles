5fe9c9c90cbfa447951b09c8aaa1003762478348
Adrià Navarrete <adri.navarrete.09@gmail.com>
2026-02-07 07:55:01 +0100

    feat: Add Docker wrapper scripts for OpenCode iterations

---
+++ bin/ralph/aliases.zsh
@@ -0,0 +1,15 @@
+ralph_claude() {
+  "$HOME/.dotfiles/bin/ralph/ralph_claude_docker.sh" "$@"
+}
+
+ralph_gemini() {
+  "$HOME/.dotfiles/bin/ralph/ralph_gemini_docker.sh" "$@"
+}
+
+ralph_opencode_codex() {
+  "$HOME/.dotfiles/bin/ralph/ralph_opencode_codex_docker.sh" "$@"
+}
+
+ralph_opencode_copilot() {
+  "$HOME/.dotfiles/bin/ralph/ralph_opencode_copilot_docker.sh" "$@"
+}
